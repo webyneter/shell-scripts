70cf210c48a080b183d81f07b55a4b93103f8d96
Nikita Shupeyko <webyneter@gmail.com>
2022-09-14 13:33:29 +0400

    Introduce prompting.bash

---
+++ sourced/prompting.bash
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# MIT License
+#
+# Copyright (c) 2021 Nikita Shupeyko
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+### Prompting scripts to be sourced into other scripts.
+
+confirm_or_exit() {
+  ### Request confirmation to proceed, or exit otherwise.
+  ### Based upn https://stackoverflow.com/a/226724
+
+  local message="${1} ('Y' or 'y' to confirm): "
+  local non_confirmation_exit_code="${2:-0}"
+
+  local reply
+  while true; do
+    read -p "${message}" reply
+    case "${reply}" in
+    [Yy]*)
+      break
+      ;;
+    *)
+      printf 'No confirmation has been given—exiting.'
+      exit "${non_confirmation_exit_code}"
+      ;;
+    esac
+  done
+  printf '\n'
+}
